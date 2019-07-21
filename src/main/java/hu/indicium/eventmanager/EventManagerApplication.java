ea2067c19b9717aa949b72ba770b667c7ddf3c1d
joostlek <joostlek@outlook.com>
2019-07-21 15:28:34 +0200

    Add ModelMapper

--- src/main/java/hu/indicium/eventmanager/EventManagerApplication.java
+++ src/main/java/hu/indicium/eventmanager/EventManagerApplication.java
@@ -1,13 +1,20 @@
 package hu.indicium.eventmanager;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class EventManagerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(EventManagerApplication.class, args);
+    }
+
+    @Bean
+    ModelMapper modelMapper() {
+        return new ModelMapper();
     }
 
 }
