15a01b2f85a8a40bb56e558dfd5ee335d66d92f4
joostlek <joostlek@outlook.com>
2019-07-21 13:33:48 +0200

    Finish Event class

--- src/main/java/hu/indicium/eventmanager/event/Event.java
+++ src/main/java/hu/indicium/eventmanager/event/Event.java
@@ -1,6 +1,7 @@
 package hu.indicium.eventmanager.event;
 
 import javax.persistence.*;
+import java.util.Date;
 
 @Entity
 public class Event {
@@ -8,4 +9,73 @@ public class Event {
     @SequenceGenerator(name = "event_id_generator", sequenceName = "event_seq", allocationSize = 1)
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "event_id_generator")
     private Long id;
+
+    private String name;
+
+    private String location;
+
+    private String description;
+
+    private Date startDate;
+
+    private Date endDate;
+
+    public Event() {
+    }
+
+    public Event(String name, String location, String description, Date startDate, Date endDate) {
+        this.name = name;
+        this.location = location;
+        this.description = description;
+        this.startDate = startDate;
+        this.endDate = endDate;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
 }
