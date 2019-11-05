7aaaf2ea705bb7fe7f3e2992f06bb11eaa7a3dcb
noudadrichem <info@noudadrichem.com>
2019-11-05 15:59:24 +0100

    fixed naming bug in springboot starter

--- src/main/java/hu/indicium/eventmanager/EventManagerApplication.java
+++ src/main/java/hu/indicium/eventmanager/EventManagerApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EventmanagerApplication {
+public class EventManagerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EventmanagerApplication.class, args);
+		SpringApplication.run(EventManagerApplication.class, args);
 	}
 
 }
--- src/main/java/hu/indicium/eventmanager/ServletInitializer.java
+++ src/main/java/hu/indicium/eventmanager/ServletInitializer.java
@@ -7,7 +7,7 @@ public class ServletInitializer extends SpringBootServletInitializer {
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(EventmanagerApplication.class);
+		return application.sources(EventManagerApplication.class);
 	}
 
 }
